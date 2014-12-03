48c57603ffd03a0fe97dacfdef5e28d61126b113
MasaruMiyamoto <miyamotomasaru@miyamotoyuu-no-MacBook-Air.local>
2014-12-03 12:38:29 +0900

    Initial Commit

---
+++ RumiBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RumiBoard
+//
+//  Created by MasaruMiyamoto on 2014/12/03.
+//  Copyright (c) 2014年 MasaruMiyamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RumiBoardTests/RumiBoardTests.m
@@ -0,0 +1,40 @@
+//
+//  RumiBoardTests.m
+//  RumiBoardTests
+//
+//  Created by MasaruMiyamoto on 2014/12/03.
+//  Copyright (c) 2014年 MasaruMiyamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface RumiBoardTests : XCTestCase
+
+@end
+
+@implementation RumiBoardTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
